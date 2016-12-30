3bbbd9ac96cac98defcadbfae93dab8e7a3da8d4
SunShinewyf <2632807692@qq.com>
2016-12-30 23:07:57 +0800

    add the share component

---
+++ js/components/share/share.component.tsx
@@ -0,0 +1,7 @@
+/**
+ * author:SunShinewyf
+ * date:2016-12-30
+ */
+import * as React from 'react'
+import { View, Text, Image, TouchableOpacity, Navigator, Dimensions, StyleSheet } from 'react-native'
+import * as CONST from '../../CONST'
