ad0d346394030f12e1c700901e3c424657ca52bd
SunShinewyf <2632807692@qq.com>
2017-01-01 23:11:44 +0800

    add the detail view

---
+++ js/views/detail/detail.view.tsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016 SunShine
+ * 2017-1-1
+ */
+import * as React from 'react';
+import { View, Text, Image, Dimensions, TextInput, TouchableOpacity, Navigator, StyleSheet } from 'react-native'
+import * as CONST from '../../CONST'
+
+export interface detailProps {
+    navigator: Navigator
+}
+export class DetailView extends React.Component<any, any>{
+    constructor(props: any) {
+        super(props)
+    }
+    render() {
+        return (
+            <View style={[styles.detailContainer, { width: CONST.WIDTH, height: CONST.HEIGHT }]}>
+                <Text>this is the detail view</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    detailContainer: {
+
+    }
+})
