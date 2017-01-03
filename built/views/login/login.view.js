a17ab3fd96c3993093715a8dd244ed4f18b43846
SunShinewyf <2632807692@qq.com>
2017-01-03 23:07:34 +0800

    finish the login view

--- built/views/login/login.view.js
+++ built/views/login/login.view.js
@@ -3,9 +3,11 @@
  * date:2017-01-02
  */
 import * as React from 'react';
-import { View, Image, TextInput, StyleSheet, } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import * as CONST from '../../CONST';
 const bgImg = require('../../../public/loginBg.png');
+const userImg = require('../../../public/username.png');
+const pawImg = require('../../../public/password.png');
 export class LoginView extends React.Component {
     constructor(props) {
         super(props);
@@ -13,22 +15,53 @@ export class LoginView extends React.Component {
     render() {
         return (React.createElement(View, { style: [styles.container, { width: CONST.WIDTH, height: CONST.HEIGHT }] },
             React.createElement(Image, { source: bgImg, style: { width: CONST.WIDTH, height: 250 } }),
-            React.createElement(View, { style: [styles.innerContainer] },
-                React.createElement(TextInput, { style: styles.input }))));
+            React.createElement(View, { style: [styles.innerContainer, { width: CONST.WIDTH - 80, marginTop: 40 }] },
+                React.createElement(Image, { source: userImg, style: styles.inputImg }),
+                React.createElement(TextInput, { style: [styles.input, {}], placeholder: '用户名' })),
+            React.createElement(View, { style: [styles.innerContainer, { width: CONST.WIDTH - 80, marginTop: 20 }] },
+                React.createElement(Image, { source: pawImg, style: styles.inputImg }),
+                React.createElement(TextInput, { style: [styles.input, {}], placeholder: '密码' })),
+            React.createElement(TouchableOpacity, { style: [styles.button, { width: CONST.WIDTH - 80, marginTop: 50 }] },
+                React.createElement(Text, { style: [styles.buttonText] }, "register")),
+            React.createElement(TouchableOpacity, { style: [styles.button, { width: CONST.WIDTH - 80, marginTop: 15 }] },
+                React.createElement(Text, { style: [styles.buttonText] }, "Login"))));
     }
 }
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        alignItems: 'center',
     },
     innerContainer: {
-        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        height: 50,
+        borderBottomColor: '#d9d9d9',
+        borderBottomWidth: 1,
+        flexDirection: 'row'
     },
     input: {
-        width: 200,
+        width: 300,
+        height: 50,
+        color: '#3d3d3d',
+        paddingLeft: 10
+    },
+    inputImg: {
+        width: 30,
+        height: 30,
+        marginLeft: 40
+    },
+    button: {
         height: 45,
+        backgroundColor: '#2a8ae0',
+        borderColor: '#2a8ae0',
         borderWidth: 1,
+        borderRadius: 4,
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 20,
     }
 });
--- js/views/login/login.view.tsx
+++ js/views/login/login.view.tsx
@@ -3,11 +3,13 @@
  * date:2017-01-02
  */
 import * as React from 'react';
-import { View, Text, Image, Dimensions, TextInput, TouchableOpacity, Navigator, StyleSheet, } from 'react-native'
+import { View, Text, Image, Dimensions, TextInput, TouchableOpacity, Navigator, StyleSheet } from 'react-native'
 import * as CONST from '../../CONST'
 import { FooterBar } from '../../components'
 
 const bgImg = require('../../../public/loginBg.png')
+const userImg = require('../../../public/username.png')
+const pawImg = require('../../../public/password.png')
 export interface loginProps {
     navitgator?: Navigator
 }
@@ -20,26 +22,59 @@ export class LoginView extends React.Component<loginProps, any>{
         return (
             <View style={[styles.container, { width: CONST.WIDTH, height: CONST.HEIGHT }]}>
                 <Image source={bgImg} style={{ width: CONST.WIDTH, height: 250 }} />
-                <View style={[styles.innerContainer]}>
-                    <TextInput style={styles.input} />
+                <View style={[styles.innerContainer, { width: CONST.WIDTH - 80, marginTop: 40 }]}>
+                    <Image source={userImg} style={styles.inputImg} />
+                    <TextInput style={[styles.input, {}]} placeholder={'用户名'} />
                 </View>
+                <View style={[styles.innerContainer, { width: CONST.WIDTH - 80, marginTop: 20 }]}>
+                    <Image source={pawImg} style={styles.inputImg} />
+                    <TextInput style={[styles.input, {}]} placeholder={'密码'} />
+                </View>
+                <TouchableOpacity style={[styles.button, { width: CONST.WIDTH - 80, marginTop: 50 }]}>
+                    <Text style={[styles.buttonText]}>register</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, { width: CONST.WIDTH - 80, marginTop: 15 }]}>
+                    <Text style={[styles.buttonText]}>Login</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        alignItems: 'center',
     },
     innerContainer: {
-        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        height: 50,
+        borderBottomColor: '#d9d9d9',
+        borderBottomWidth: 1,
+        flexDirection: 'row'
     },
     input: {
-        width: 200,
+        width: 300,
+        height: 50,
+        color: '#3d3d3d',
+        paddingLeft: 10
+    },
+    inputImg: {
+        width: 30,
+        height: 30,
+        marginLeft: 40
+    },
+    button: {
         height: 45,
+        backgroundColor: '#2a8ae0',
+        borderColor: '#2a8ae0',
         borderWidth: 1,
-
+        borderRadius: 4,
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 20,
     }
 })
